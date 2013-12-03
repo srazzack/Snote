fa102f596d8d310847185164c5e3aa2af72a88be
srazzack <srazzack@gmail.com>
2013-12-03 14:07:15 -0800

    slideshow update 2

--- public/src/views.slideshow.js
+++ public/src/views.slideshow.js
@@ -1,45 +1,57 @@
 var SlideShowView = Backbone.View.extend({
  
-  el: '#slideshow',
+  el: "#app",
+  currentSlide: 0,
+  templates: {
+      slideShow: _.template($("#slideShowTemplate").html())
+  },
 
   initialize: function() {
-        if(this.$el.requestFullScreen) {
-            this.$el.requestFullScreen();
-        } 
-        else if(this.$el.mozRequestFullScreen) {
-            console.log(moz);
-            this.$el.mozRequestFullScreen();
-        } 
-        else if(this.$el.webkitRequestFullScreen) {
-            console.log("webkit");
-            this.$el.webkitRequestFullScreen();
-        }
-        else{
-          console.log("something is wrong with use of the Fullscreen API");
-        }
+      console.log("here we are in the slideshowview");
   }, 
 
-  events: {
-  },
- 
-  slideTemplate: _.template(),
- 
-  controlTemplate: _.template(
-  ),
- 
-
- 
   render: function() {
+      console.log(this.collection.at(this.currentSlide));
+      this.$el.find("#slideShow").html(this.templates["slideShow"](this.collection.at(this.currentSlide)));
+
+      if(this.$el.find("#slideShow").requestFullScreen) {
+          this.$el.find("#slideShow").requestFullScreen();
+      } 
+      else if(this.$el.find("#slideShow").mozRequestFullScreen) {
+          console.log(moz);
+          this.$el.find("#slideShow").mozRequestFullScreen();
+      } 
+      else if(this.$el.find("#slideShow").webkitRequestFullScreen) {
+          console.log("webkit");
+          this.$el.find("#slideShow").webkitRequestFullScreen();
+      }
+      else{
+          console.log("something is wrong with use of the Fullscreen API");
+      }
+  },
+
+  events: {
+      "#prevSlide":"showPrevious",
+      "#nextSlide":"showNext",
+      "#playSlideShow":"play"
+  },
+
+  getSlide: function(e){
+      return this.collection.get($(e.currentTarget).attr("data-id"));
+  },
+
+  showPrevious: function(e){
+
+  },
+  
+  showNext: function(e){
+
   },
  
-  transition: function() {
+  play: function(e) {
   },
  
- 
-  play: function() {
-  },
- 
-  pause: function() {
+  pause: function(e) {
   }
 
 });
