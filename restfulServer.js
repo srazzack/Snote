6cd392c01f48f3e0a87f19f011604210f2c1c90d
srazzack <srazzack@gmail.com>
2013-12-08 17:22:45 -0800

    update on restFullServer

--- restfulServer.js
+++ restfulServer.js
@@ -5,15 +5,33 @@ var app  	= express();
 var _    	= require('underscore');
 var Slide	= require('slide');
 
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    //define the root directory for static files
+    app.use(express.static(__dirname + '/public'));
+    app.use(app.router);
+});
+
+app.configure('development', function(){
+    app.use(express.logger());
+    app.use(express.errorHandler({
+    	dumpExceptions: true,
+    	showStack	  : true
+    }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+app.get('/', function(request, response){ 
+	response.redirect('/index.html');
+});
 app.get('/slide', function(request, response){
 	response.send(Slide.get());
 });
 
-app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-	app.use(app.router);
-});
 
 app.get('/slide/:title', function(request, response){
 	console.log("this request: ", request.url);
