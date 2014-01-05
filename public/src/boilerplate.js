6e5f3f068fa4a9dbf65eac66773720f9c426ef4b
srazzack <srazzack@gmail.com>
2014-01-04 16:03:18 -0800

    imoplementing requirejs phase 1

---
+++ public/src/boilerplate.js
@@ -0,0 +1,14 @@
+//Filename: boilerplate.js
+
+define([
+  // These are path alias that configured in the bootstrap
+  'jquery',     
+  'underscore', 
+  'backbone',
+  'inline'    
+], function($, _, Backbone){
+  // Above we have passed in jQuery, Underscore and Backbone
+  // They will not be accessible in the global scope
+  return {};
+  // What we return here will be used by other modules
+});
